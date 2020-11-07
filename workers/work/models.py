03b4d8645a2615d8f5fafbb1d9417e7f624729c7
Mogrima <rebel.rs@yandex.ru>
2020-11-08 01:52:01 +0400

    Создан проект с приложением work и моделью базы данных. Данные добавлены в таблицу

---
+++ workers/work/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Workers(models.Model):
+    name = models.CharField(max_length=200)
+    companyName = models.CharField(max_length=100)
+    position_name = models.CharField(max_length=100)
+    hire_date = models.DateField('date hire_date', auto_now=False)
+    fire_date = models.DateField('date fire_date', auto_now=False, null=True, blank=True)
+    salary = models.IntegerField()
+    fraction = models.IntegerField()
+    base = models.IntegerField()
+    advance = models.IntegerField()
+    by_hours = models.BooleanField()
+    def __str__(self):
+        return self.name
