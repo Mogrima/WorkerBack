00ee42fd8168dce42d8d2605784be0ee3795e0f3
Mogrima <rebel.rs@yandex.ru>
2020-11-11 01:10:24 +0400

    немного изменено представление work

--- workers/work/views.py
+++ workers/work/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Workers
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the work index.")
+    workers_list = Workers.objects.order_by('id')[:5]
+    output = ', '.join([q.name for q in workers_list])
+    return HttpResponse(output)
